e93eb5cd82e05eac5c4948cfd91477bee347d196
paku <boardguy1024@gmail.com>
2023-10-23 21:53:06 +0900

    scrollによるheaderAnimationを実装

--- ProfileHeaderLikeTwitter/View/Home.swift
+++ ProfileHeaderLikeTwitter/View/Home.swift
@@ -10,18 +10,16 @@ import SwiftUI
 struct Home: View {
     
     @State var offset: CGFloat = 0
-    
+    let screenWidth = UIScreen.main.bounds.width
+    let headerImageHeight: CGFloat = 170
+
     var body: some View {
         
         ScrollView(.vertical, showsIndicators: false) {
             VStack(spacing: 15) {
                 GeometryReader { proxy -> AnyView in
-                    
                     let minY = proxy.frame(in: .global).minY
                     print(minY)
-                    
-                    let screenWidth = UIScreen.main.bounds.width
-                    let height: CGFloat = 180
                     //これを囲まないと
                     //「Modifying state during view update, this will cause undefined behavior.」警告がでる
                     // 要は、scrollすると minY -> @State offsetに新しい値が代入され
@@ -31,24 +29,84 @@ struct Home: View {
                         self.offset = minY
                     }
                     
-                    return AnyView(
-                        ZStack {
-                            Image("tesla")
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: screenWidth, 
-                                       height: minY > 0 ? height + minY : height, alignment: .center)
-                        }
-                        // 下にスクロール時はheaderを固定
-                        .offset(y: minY > 0 ? -minY : 0)
-                        
-                        
-                    )
-                    
+                    return headerImage
                 }
+                .frame(height: headerImageHeight)
+                .zIndex(-offset > 80 ? 1 : 0)
+                
+                // MARK: - Profile Icon Image
+                HStack {
+                   
+                    profileIcon
+                        // scroll量が80を超えたら 0.5のスケールを保つ
+                        .scaleEffect(-offset > 80 ? 0.5 :
+                                        
+                                      // 下にscrollの場合スケールは1 それ以外、上にscrollした量が80まで、scrollの半分の量(1 * 0.5)でアイコンを縮小
+                                      offset > 0 ? 1 : 1 - ((-offset / 80) * 0.5))
+                    
+                    Spacer()
+                    
+                    editButton
+                        .offset(y: 5)
+                }
+                .padding(.horizontal)
+                .padding(.vertical, -30)
+                .zIndex(0)
             }
         }
         .ignoresSafeArea()
+    }
+}
+
+extension Home {
+    var headerImage: AnyView {
+        AnyView(
+            // ----------------------------------------------------
+            Image("tesla")
+                .resizable()
+                .scaledToFill()
+                .frame(width: screenWidth,
+                       // 下にscrollした分 heightの高さをincrementする
+                       height: offset > 0 ? headerImageHeight + offset : headerImageHeight, alignment: .center)
+                .clipShape(Rectangle())
+            // 下にスクロール時はheaderを固定
+            .offset(y:
+                        // scroll down offset: 相殺して　y:0 positionを維持
+                        offset > 0 ?  -offset :
+                       
+                        // scroll up
+                        // (offset > -80 ? 0) 80までは offsetを0にしているので headerImageはScrollViewのスクロールに合わせて動く
+                        // 80を超えた場合 (-80 - offset) y:-80の位置から （scrollした分 - offset分)で相殺し -80ポジションを維持させる
+                        offset > -80 ? 0 : -80 - offset)
+        )
+        
+    }
+    
+    var profileIcon: some View {
+        Image("profile")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 75, height: 75)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 6))
+            .zIndex(0)
+
+    }
+    
+    var editButton: some View {
+        Button {
+            
+        } label: {
+            Text("Edit Profile")
+                .bold()
+                .foregroundStyle(.white)
+                .padding(.vertical, 8)
+                .padding(.horizontal, 30)
+                .background(
+                    Capsule()
+                        .fill(.black)
+                )
+        }
     }
 }
 
