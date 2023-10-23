d06e8520e126dd8c93cef0fe1a34a4adc9d0e919
paku <boardguy1024@gmail.com>
2023-10-23 18:28:54 +0900

    Initial Commit

---
+++ ProfileHeaderLikeTwitter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProfileHeaderLikeTwitter
+//
+//  Created by paku on 2023/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProfileHeaderLikeTwitter/ProfileHeaderLikeTwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileHeaderLikeTwitterApp.swift
+//  ProfileHeaderLikeTwitter
+//
+//  Created by paku on 2023/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ProfileHeaderLikeTwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
